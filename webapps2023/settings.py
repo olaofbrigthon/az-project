2c8ada7234c659deaaadf42854e052321c25c4ca
olaofbrigthon <168484094+olaofbrigthon@users.noreply.github.com>
2024-05-01 08:08:10 +0100

    Update settings.py

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'webapps2023.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'server3',
+        'User': 'adim',
+        'PASSWORD': 'Moyosore123'
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
